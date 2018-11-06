061d6ee94fedba72134d17910b598070409dd32d
Daniel Tovesson <daniel.tovesson@me.com>
2018-11-06 10:03:01 +0100

    Lab 3: format code

--- Lab 3/Golomb.java
+++ Lab 3/Golomb.java
@@ -1,9 +1,10 @@
+
 /**
- *  Golomb.java 
+ *  Golomb.java
  *  This file is part of JaCoP.
  *
- *  JaCoP is a Java Constraint Programming solver. 
- *	
+ *  JaCoP is a Java Constraint Programming solver.
+ *
  *	Copyright (C) 2000-2008 Krzysztof Kuchcinski and Radoslaw Szymanek
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -15,7 +16,7 @@
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Affero General Public License for more details.
- *  
+ *
  *  Notwithstanding any other provision of this License, the copyright
  *  owners of this work supplement the terms of this License with terms
  *  prohibiting misrepresentation of the origin of this work and requiring
@@ -29,7 +30,6 @@
  *
  */
 
-
 import java.util.ArrayList;
 
 import org.jacop.constraints.Alldiff;
@@ -40,144 +40,135 @@ import org.jacop.constraints.XplusClteqZ;
 import org.jacop.constraints.XplusYeqZ;
 import org.jacop.core.IntVar;
 import org.jacop.core.Store;
-import org.jacop.search.DepthFirstSearch;
-import org.jacop.search.IndomainMin;
-import org.jacop.search.InputOrderSelect;
-import org.jacop.search.PrintOutListener;
-import org.jacop.search.Search;
-import org.jacop.search.SelectChoicePoint;
 
 /**
- * 
+ *
  * It models a Golomb ruler problem.
- * 
+ *
  * @author Radoslaw Szymanek and Krzysztof Kuchcinski
  *
- * 
- * Golomb ruler is a special sequence of natural numbers
- * an example is 0 1 4 6
  *
- * a sequence is a Golomb ruler if all differences are different
- * (1-0), (4-0), (6-0), (4-1), (6-1), (6-4)
- * 1 4 6 3 5 2
- * All differences above have different values
- * A Golomb ruler is optimal if the length of it (the last mark)
- * has the smallest possible value
- * The presented ruler with 4 marks of length 6 is optimal
+ *         Golomb ruler is a special sequence of natural numbers an example is 0
+ *         1 4 6
+ *
+ *         a sequence is a Golomb ruler if all differences are different (1-0),
+ *         (4-0), (6-0), (4-1), (6-1), (6-4) 1 4 6 3 5 2 All differences above
+ *         have different values A Golomb ruler is optimal if the length of it
+ *         (the last mark) has the smallest possible value The presented ruler
+ *         with 4 marks of length 6 is optimal
  */
 
 public class Golomb {
 
-    Store store;
+	Store store;
 
-    /**
-     * It specifies the number of marks (number of natural numbers in 
-     * the sequence).
-     */
-    public int noMarks = 10;
+	/**
+	 * It specifies the number of marks (number of natural numbers in the
+	 * sequence).
+	 */
+	public int noMarks = 10;
 
-    /**
-     * It specifies the upper bound of the optimal solution.
-     */
-    public int bound = -1;
+	/**
+	 * It specifies the upper bound of the optimal solution.
+	 */
+	public int bound = -1;
 
-	
-    /**
-     * It contains all differences between all possible pairs of marks.
-     */
-    public ArrayList<IntVar> subs = new ArrayList<IntVar>();
-	
-    /**
-     * It executes the program which computes the optimal Golomb ruler. 
-     * 
-     * @param args the first argument specifies the number of marks, the second argument specifies the upper bound of the optimal solution.
-     */
-    public static void main(String args[]) {
-		
-	Golomb example = new Golomb();
-		
-	example.model();
+	/**
+	 * It contains all differences between all possible pairs of marks.
+	 */
+	public ArrayList<IntVar> subs = new ArrayList<IntVar>();
 
-    }			
+	/**
+	 * It executes the program which computes the optimal Golomb ruler.
+	 *
+	 * @param args
+	 *            the first argument specifies the number of marks, the second
+	 *            argument specifies the upper bound of the optimal solution.
+	 */
+	public static void main(String args[]) {
 
-    public void model() {
+		Golomb example = new Golomb();
 
-	System.out.println("Program to solve Golomb mark problem - length "
-			   + noMarks);
+		example.model();
 
-	store = new Store();
-	ArrayList<IntVar> vars = new ArrayList<IntVar>();
-		
-	IntVar numbers[] = new IntVar[noMarks];
-
-	for (int i = 0; i < numbers.length; i++) {
-	    // Create FDV for each natural number
-	    numbers[i] = new IntVar(store, "n" + new Integer(i), (i + 1) * i / 2,
-				    noMarks * noMarks);
-
-	    // Impose constraints that each consequtive number
-	    // is larger than the previous one
-	    // Golomb ruler is an ordered sequence of numbers
-	    if (i > 0)
-		store.impose(new XltY(numbers[i - 1], numbers[i]));
-	    else
-		store.impose(new XeqC(numbers[0], 0));
 	}
 
-	for (IntVar v : numbers)
-	    vars.add(v);
-		
-	if (bound > -1)
-	    store.impose(new XlteqC(numbers[noMarks - 1], bound));
+	public void model() {
 
-	// ArrayList contains all differences
-	subs = new ArrayList<IntVar>();
+		System.out.println("Program to solve Golomb mark problem - length " + noMarks);
 
-	for (int i = 1; i < numbers.length; i++) {
+		store = new Store();
+		ArrayList<IntVar> vars = new ArrayList<IntVar>();
 
-	    // for (int j = i - 1; j >= 0; j--) {
-	    for (int j = 0; j < i; j++) {
-		// Create FDV for a difference between ith and jth number
-		IntVar sub = new IntVar(store, "c" + i + "_" + j, (i - j)
-					* (i - j + 1) / 2, noMarks * noMarks);
+		IntVar numbers[] = new IntVar[noMarks];
 
-		subs.add(sub);
+		for (int i = 0; i < numbers.length; i++) {
+			// Create FDV for each natural number
+			numbers[i] = new IntVar(store, "n" + new Integer(i), (i + 1) * i / 2, noMarks * noMarks);
 
-		// sub + jth = ith since sub = ith - jth
-		// Add constraint so the above relationship holds
-		//			store.imposePropagators(new XplusYeqZ(sub, numbers[j], numbers[i]));
-		store.impose(new XplusYeqZ(sub, numbers[j], numbers[i]));
+			// Impose constraints that each consequtive number
+			// is larger than the previous one
+			// Golomb ruler is an ordered sequence of numbers
+			if (i > 0)
+				store.impose(new XltY(numbers[i - 1], numbers[i]));
+			else
+				store.impose(new XeqC(numbers[0], 0));
+		}
 
-	    }
+		for (IntVar v : numbers)
+			vars.add(v);
+
+		if (bound > -1)
+			store.impose(new XlteqC(numbers[noMarks - 1], bound));
+
+		// ArrayList contains all differences
+		subs = new ArrayList<IntVar>();
+
+		for (int i = 1; i < numbers.length; i++) {
+
+			// for (int j = i - 1; j >= 0; j--) {
+			for (int j = 0; j < i; j++) {
+				// Create FDV for a difference between ith and jth number
+				IntVar sub = new IntVar(store, "c" + i + "_" + j, (i - j) * (i - j + 1) / 2, noMarks * noMarks);
+
+				subs.add(sub);
+
+				// sub + jth = ith since sub = ith - jth
+				// Add constraint so the above relationship holds
+				// store.imposePropagators(new XplusYeqZ(sub, numbers[j],
+				// numbers[i]));
+				store.impose(new XplusYeqZ(sub, numbers[j], numbers[i]));
+
+			}
+		}
+
+		int index = 0;
+		for (int i = 1; i < noMarks; i++)
+			for (int j = 0; j < i; j++)
+				// store.imposePropagators(new XplusClteqZ(subs.get(index++),
+				// (noMarks - 1 - i + j)
+				// * (noMarks - i + j) / 2, numbers[noMarks - 1]));
+				store.impose(new XplusClteqZ(subs.get(index++), (noMarks - 1 - i + j) * (noMarks - i + j) / 2,
+						numbers[noMarks - 1]));
+
+		// symmetry breaking constraint
+		// important constraint to reduce search space since
+		// interested in proving the optimality
+		store.impose(new XltY(subs.get(0), subs.get(subs.size() - 1)));
+
+		// All differences have to have unique values
+		store.impose(new Alldiff(subs), 1);
+
+		IntVar cost = numbers[numbers.length - 1];
+
+		SimpleDFS search = new SimpleDFS(store);
+		search.setVariablesToReport(numbers);
+		search.setCostVariable(cost);
+
+		boolean result = search.label(numbers);
+
+		System.out.println(result);
+
 	}
 
-	int index = 0;
-	for (int i = 1; i < noMarks; i++)
-	    for (int j = 0; j < i; j++)
-		//				store.imposePropagators(new XplusClteqZ(subs.get(index++), (noMarks - 1 - i + j)
-		//						* (noMarks - i + j) / 2, numbers[noMarks - 1]));
-		store.impose(new XplusClteqZ(subs.get(index++), (noMarks - 1 - i + j)
-					     * (noMarks - i + j) / 2, numbers[noMarks - 1]));
-
-	// symmetry breaking constraint
-	// important constraint to reduce search space since
-	// interested in proving the optimality
-	store.impose(new XltY(subs.get(0), subs.get(subs.size() - 1)));
-
-	// All differences have to have unique values
-	store.impose(new Alldiff(subs), 1);
-
-	IntVar cost = numbers[numbers.length - 1];
-
-	SimpleDFS search = new SimpleDFS(store);
-	search.setVariablesToReport(numbers);
-	search.setCostVariable(cost);
-
-	boolean result = search.label(numbers);
-	
-	System.out.println (result);
-
-    }
-	
-	
 }
--- Lab 3/SimpleDFS.java
+++ Lab 3/SimpleDFS.java
@@ -1,9 +1,9 @@
 /**
- *  SimpleDFS.java 
+ *  SimpleDFS.java
  *  This file is part of JaCoP.
  *
- *  JaCoP is a Java Constraint Programming solver. 
- *	
+ *  JaCoP is a Java Constraint Programming solver.
+ *
  *	Copyright (C) 2000-2008 Krzysztof Kuchcinski and Radoslaw Szymanek
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -15,7 +15,7 @@
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Affero General Public License for more details.
- *  
+ *
  *  Notwithstanding any other provision of this License, the copyright
  *  owners of this work supplement the terms of this License with terms
  *  prohibiting misrepresentation of the origin of this work and requiring
@@ -39,203 +39,203 @@ import org.jacop.core.Store;
 
 /**
  * Implements Simple Depth First Search .
- * 
+ *
  * @author Krzysztof Kuchcinski
  * @version 4.1
  */
 
-public class SimpleDFS  {
+public class SimpleDFS {
 
-    boolean trace = false;
-
-    /**
-     * Store used in search
-     */
-    Store store;
-
-    /**
-     * Defines varibales to be printed when solution is found
-     */
-    IntVar[] variablesToReport;
-
-    /**
-     * It represents current depth of store used in search.
-     */
-    int depth = 0;
-
-    /**
-     * It represents the cost value of currently best solution for FloatVar cost.
-     */
-    public int costValue = IntDomain.MaxInt;
-
-    /**
-     * It represents the cost variable.
-     */
-    public IntVar costVariable = null;
-
-    public SimpleDFS(Store s) {
-	store = s;
-    }
-
-
-    /**
-     * This function is called recursively to assign variables one by one.
-     */
-    public boolean label(IntVar[] vars) {
-
-	if (trace) {
-	    for (int i = 0; i < vars.length; i++) 
-		System.out.print (vars[i] + " ");
-	    System.out.println ();
-	}
-
-	ChoicePoint choice = null;
-	boolean consistent;
-
-	// Instead of imposing constraint just restrict bounds
-	// -1 since costValue is the cost of last solution
-	if (costVariable != null) {
-	    try {
-		if (costVariable.min() <= costValue - 1)
-		    costVariable.domain.in(store.level, costVariable, costVariable.min(), costValue - 1);
-		else
-		    return false;
-	    } catch (FailException f) {
-		return false;
-	    }
-	}
-
-	consistent = store.consistency();
-		
-	if (!consistent) {
-	    // Failed leaf of the search tree
-	    return false;
-	} else { // consistent
-
-	    if (vars.length == 0) {
-		// solution found; no more variables to label
-
-		// update cost if minimization
-		if (costVariable != null)
-		    costValue = costVariable.min();
-
-		reportSolution();
-
-		return costVariable == null; // true is satisfiability search and false if minimization
-	    }
-
- 	    choice = new ChoicePoint(vars);
-
-	    levelUp();
-
-	    store.impose(choice.getConstraint());
-
-	    // choice point imposed.
-			
-	    consistent = label(choice.getSearchVariables());
-
-            if (consistent) {
-		levelDown();
-		return true;
-	    } else {
-
-		restoreLevel();
-
-		store.impose(new Not(choice.getConstraint()));
-
-		// negated choice point imposed.
-		
-		consistent = label(vars);
-
-		levelDown();
-
-		if (consistent) {
-		    return true;
-		} else {
-		    return false;
-		}
-	    }
-	}
-    }
-
-    void levelDown() {
-	store.removeLevel(depth);
-	store.setLevel(--depth);
-    }
-
-    void levelUp() {
-	store.setLevel(++depth);
-    }
-
-    void restoreLevel() {
-	store.removeLevel(depth);
-	store.setLevel(store.level);
-    }
-
-    public void reportSolution() {
-	if (costVariable != null)
-	    System.out.println ("Cost is " + costVariable);
-
-	for (int i = 0; i < variablesToReport.length; i++) 
-	    System.out.print (variablesToReport[i] + " ");
-	System.out.println ("\n---------------");
-    }
-
-    public void setVariablesToReport(IntVar[] v) {
-	variablesToReport = v;
-    }
-
-    public void setCostVariable(IntVar v) {
-	costVariable = v;
-    }
-
-    public class ChoicePoint {
-
-	IntVar var;
-	IntVar[] searchVariables;
-	int value;
-
-	public ChoicePoint (IntVar[] v) {
-	    var = selectVariable(v);
-	    value = selectValue(var);
-	}
-
-	public IntVar[] getSearchVariables() {
-	    return searchVariables;
-	}
+	boolean trace = false;
 
 	/**
-	 * example variable selection; input order
-	 */ 
-	IntVar selectVariable(IntVar[] v) {
-	    if (v.length != 0) {
-
-		searchVariables = new IntVar[v.length-1];
-		for (int i = 0; i < v.length-1; i++) {
-		    searchVariables[i] = v[i+1]; 
-		}
-
-		return v[0];
-
-	    }
-	    else {
-		System.err.println("Zero length list of variables for labeling");
-		return new IntVar(store);
-	    }
-	}
-
-	/**
-	 * example value selection; indomain_min
-	 */ 
-	int selectValue(IntVar v) {
-	    return v.min();
-	}
-
-	/**
-	 * example constraint assigning a selected value
+	 * Store used in search
 	 */
-	public PrimitiveConstraint getConstraint() {
-	    return new XeqC(var, value);
+	Store store;
+
+	/**
+	 * Defines varibales to be printed when solution is found
+	 */
+	IntVar[] variablesToReport;
+
+	/**
+	 * It represents current depth of store used in search.
+	 */
+	int depth = 0;
+
+	/**
+	 * It represents the cost value of currently best solution for FloatVar
+	 * cost.
+	 */
+	public int costValue = IntDomain.MaxInt;
+
+	/**
+	 * It represents the cost variable.
+	 */
+	public IntVar costVariable = null;
+
+	public SimpleDFS(Store s) {
+		store = s;
 	}
-    }
+
+	/**
+	 * This function is called recursively to assign variables one by one.
+	 */
+	public boolean label(IntVar[] vars) {
+
+		if (trace) {
+			for (int i = 0; i < vars.length; i++)
+				System.out.print(vars[i] + " ");
+			System.out.println();
+		}
+
+		ChoicePoint choice = null;
+		boolean consistent;
+
+		// Instead of imposing constraint just restrict bounds
+		// -1 since costValue is the cost of last solution
+		if (costVariable != null) {
+			try {
+				if (costVariable.min() <= costValue - 1)
+					costVariable.domain.in(store.level, costVariable, costVariable.min(), costValue - 1);
+				else
+					return false;
+			} catch (FailException f) {
+				return false;
+			}
+		}
+
+		consistent = store.consistency();
+
+		if (!consistent) {
+			// Failed leaf of the search tree
+			return false;
+		} else { // consistent
+
+			if (vars.length == 0) {
+				// solution found; no more variables to label
+
+				// update cost if minimization
+				if (costVariable != null)
+					costValue = costVariable.min();
+
+				reportSolution();
+
+				return costVariable == null; // true is satisfiability search
+												// and false if minimization
+			}
+
+			choice = new ChoicePoint(vars);
+
+			levelUp();
+
+			store.impose(choice.getConstraint());
+
+			// choice point imposed.
+
+			consistent = label(choice.getSearchVariables());
+
+			if (consistent) {
+				levelDown();
+				return true;
+			} else {
+
+				restoreLevel();
+
+				store.impose(new Not(choice.getConstraint()));
+
+				// negated choice point imposed.
+
+				consistent = label(vars);
+
+				levelDown();
+
+				if (consistent) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+	}
+
+	void levelDown() {
+		store.removeLevel(depth);
+		store.setLevel(--depth);
+	}
+
+	void levelUp() {
+		store.setLevel(++depth);
+	}
+
+	void restoreLevel() {
+		store.removeLevel(depth);
+		store.setLevel(store.level);
+	}
+
+	public void reportSolution() {
+		if (costVariable != null)
+			System.out.println("Cost is " + costVariable);
+
+		for (int i = 0; i < variablesToReport.length; i++)
+			System.out.print(variablesToReport[i] + " ");
+		System.out.println("\n---------------");
+	}
+
+	public void setVariablesToReport(IntVar[] v) {
+		variablesToReport = v;
+	}
+
+	public void setCostVariable(IntVar v) {
+		costVariable = v;
+	}
+
+	public class ChoicePoint {
+
+		IntVar var;
+		IntVar[] searchVariables;
+		int value;
+
+		public ChoicePoint(IntVar[] v) {
+			var = selectVariable(v);
+			value = selectValue(var);
+		}
+
+		public IntVar[] getSearchVariables() {
+			return searchVariables;
+		}
+
+		/**
+		 * example variable selection; input order
+		 */
+		IntVar selectVariable(IntVar[] v) {
+			if (v.length != 0) {
+
+				searchVariables = new IntVar[v.length - 1];
+				for (int i = 0; i < v.length - 1; i++) {
+					searchVariables[i] = v[i + 1];
+				}
+
+				return v[0];
+
+			} else {
+				System.err.println("Zero length list of variables for labeling");
+				return new IntVar(store);
+			}
+		}
+
+		/**
+		 * example value selection; indomain_min
+		 */
+		int selectValue(IntVar v) {
+			return v.min();
+		}
+
+		/**
+		 * example constraint assigning a selected value
+		 */
+		public PrimitiveConstraint getConstraint() {
+			return new XeqC(var, value);
+		}
+	}
 }
