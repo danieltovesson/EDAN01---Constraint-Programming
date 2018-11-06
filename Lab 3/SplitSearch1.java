c8e532e291540a552d3b45d70fa2c12b30a7ff66
Daniel Tovesson <daniel.tovesson@me.com>
2018-11-06 13:04:04 +0100

    Lab 3: format code

--- Lab 3/SplitSearch1.java
+++ Lab 3/SplitSearch1.java
@@ -45,205 +45,205 @@ import org.jacop.core.Store;
 
 public class SplitSearch1 {
 
-    int totalNodes = 0;
-    int nbrWrongDecisions = 0;
+	int totalNodes = 0;
+	int nbrWrongDecisions = 0;
 
-    boolean trace = false;
+	boolean trace = false;
 
-    /**
-     * Store used in search
-     */
-    Store store;
+	/**
+	 * Store used in search
+	 */
+	Store store;
 
-    /**
-     * Defines varibales to be printed when solution is found
-     */
-    IntVar[] variablesToReport;
+	/**
+	 * Defines varibales to be printed when solution is found
+	 */
+	IntVar[] variablesToReport;
 
-    /**
-     * It represents current depth of store used in search.
-     */
-    int depth = 0;
+	/**
+	 * It represents current depth of store used in search.
+	 */
+	int depth = 0;
 
-    /**
-     * It represents the cost value of currently best solution for FloatVar
-     * cost.
-     */
-    public int costValue = IntDomain.MaxInt;
+	/**
+	 * It represents the cost value of currently best solution for FloatVar
+	 * cost.
+	 */
+	public int costValue = IntDomain.MaxInt;
 
-    /**
-     * It represents the cost variable.
-     */
-    public IntVar costVariable = null;
+	/**
+	 * It represents the cost variable.
+	 */
+	public IntVar costVariable = null;
 
-    public SplitSearch1(Store s) {
-        store = s;
-    }
+	public SplitSearch1(Store s) {
+		store = s;
+	}
 
-    /**
-     * This function is called recursively to assign variables one by one.
-     */
-    public boolean label(IntVar[] vars) {
+	/**
+	 * This function is called recursively to assign variables one by one.
+	 */
+	public boolean label(IntVar[] vars) {
 
-        if (trace) {
-            for (int i = 0; i < vars.length; i++)
-                System.out.print(vars[i] + " ");
-            System.out.println();
-        }
+		if (trace) {
+			for (int i = 0; i < vars.length; i++)
+				System.out.print(vars[i] + " ");
+			System.out.println();
+		}
 
-        ChoicePoint choice = null;
-        boolean consistent;
+		ChoicePoint choice = null;
+		boolean consistent;
 
-        // Instead of imposing constraint just restrict bounds
-        // -1 since costValue is the cost of last solution
-        if (costVariable != null) {
-            try {
-                if (costVariable.min() <= costValue - 1)
-                    costVariable.domain.in(store.level, costVariable, costVariable.min(), costValue - 1);
-                else
-                    return false;
-            } catch (FailException f) {
-                return false;
-            }
-        }
+		// Instead of imposing constraint just restrict bounds
+		// -1 since costValue is the cost of last solution
+		if (costVariable != null) {
+			try {
+				if (costVariable.min() <= costValue - 1)
+					costVariable.domain.in(store.level, costVariable, costVariable.min(), costValue - 1);
+				else
+					return false;
+			} catch (FailException f) {
+				return false;
+			}
+		}
 
-        consistent = store.consistency();
+		consistent = store.consistency();
 
-        totalNodes++;
+		totalNodes++;
 
-        if (!consistent) {
-            // Failed leaf of the search tree
-            nbrWrongDecisions++;
-            return false;
-        } else { // consistent
+		if (!consistent) {
+			// Failed leaf of the search tree
+			nbrWrongDecisions++;
+			return false;
+		} else { // consistent
 
-            if (vars.length == 0) {
-                // solution found; no more variables to label
+			if (vars.length == 0) {
+				// solution found; no more variables to label
 
-                // update cost if minimization
-                if (costVariable != null)
-                    costValue = costVariable.min();
+				// update cost if minimization
+				if (costVariable != null)
+					costValue = costVariable.min();
 
-                reportSolution();
+				reportSolution();
 
-                return costVariable == null; // true is satisfiability search
-                // and false if minimization
-            }
+				return costVariable == null; // true is satisfiability search
+				// and false if minimization
+			}
 
-            choice = new ChoicePoint(vars);
+			choice = new ChoicePoint(vars);
 
-            levelUp();
+			levelUp();
 
-            store.impose(choice.getConstraint());
+			store.impose(choice.getConstraint());
 
-            // choice point imposed.
+			// choice point imposed.
 
-            consistent = label(choice.getSearchVariables());
+			consistent = label(choice.getSearchVariables());
 
-            if (consistent) {
-                levelDown();
-                return true;
-            } else {
+			if (consistent) {
+				levelDown();
+				return true;
+			} else {
 
-                restoreLevel();
+				restoreLevel();
 
-                store.impose(new Not(choice.getConstraint()));
+				store.impose(new Not(choice.getConstraint()));
 
-                // negated choice point imposed.
+				// negated choice point imposed.
 
-                consistent = label(vars);
+				consistent = label(vars);
 
-                levelDown();
+				levelDown();
 
-                if (consistent) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-    }
+				if (consistent) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+	}
 
-    void levelDown() {
-        store.removeLevel(depth);
-        store.setLevel(--depth);
-    }
+	void levelDown() {
+		store.removeLevel(depth);
+		store.setLevel(--depth);
+	}
 
-    void levelUp() {
-        store.setLevel(++depth);
-    }
+	void levelUp() {
+		store.setLevel(++depth);
+	}
 
-    void restoreLevel() {
-        store.removeLevel(depth);
-        store.setLevel(store.level);
-    }
+	void restoreLevel() {
+		store.removeLevel(depth);
+		store.setLevel(store.level);
+	}
 
-    public void reportSolution() {
-        if (costVariable != null)
-            System.out.println("Cost is " + costVariable);
-        System.out.println("Total nodes: " + totalNodes);
-        System.out.println("Wrong descisions: " + nbrWrongDecisions);
+	public void reportSolution() {
+		if (costVariable != null)
+			System.out.println("Cost is " + costVariable);
+		System.out.println("Total nodes: " + totalNodes);
+		System.out.println("Wrong descisions: " + nbrWrongDecisions);
 
-        for (int i = 0; i < variablesToReport.length; i++)
-            System.out.print(variablesToReport[i] + " ");
-        System.out.println("\n---------------");
-    }
+		for (int i = 0; i < variablesToReport.length; i++)
+			System.out.print(variablesToReport[i] + " ");
+		System.out.println("\n---------------");
+	}
 
-    public void setVariablesToReport(IntVar[] v) {
-        variablesToReport = v;
-    }
+	public void setVariablesToReport(IntVar[] v) {
+		variablesToReport = v;
+	}
 
-    public void setCostVariable(IntVar v) {
-        costVariable = v;
-    }
+	public void setCostVariable(IntVar v) {
+		costVariable = v;
+	}
 
-    public class ChoicePoint {
+	public class ChoicePoint {
 
-        IntVar var;
-        IntVar[] searchVariables;
-        int c;
+		IntVar var;
+		IntVar[] searchVariables;
+		int c;
 
-        public ChoicePoint(IntVar[] v) {
-            var = selectVariable(v);
-            c = calculateMiddlePoint(var);
-        }
+		public ChoicePoint(IntVar[] v) {
+			var = selectVariable(v);
+			c = calculateMiddlePoint(var);
+		}
 
-        public IntVar[] getSearchVariables() {
-            return searchVariables;
-        }
+		public IntVar[] getSearchVariables() {
+			return searchVariables;
+		}
 
-        /**
-         * example variable selection; input order
-         */
-        IntVar selectVariable(IntVar[] v) {
-            if (v.length != 0) {
-                if (v[0].min() == v[0].max()) {
-                    searchVariables = new IntVar[v.length - 1];
-                    for (int i = 0; i < v.length - 1; i++) {
-                        searchVariables[i] = v[i + 1];
-                    }
-                } else {
-                    searchVariables = v;
-                }
-                return v[0];
-            } else {
-                System.err.println("Zero length list of variables for labeling");
-                return new IntVar(store);
-            }
-        }
+		/**
+		 * example variable selection; input order
+		 */
+		IntVar selectVariable(IntVar[] v) {
+			if (v.length != 0) {
+				if (v[0].min() == v[0].max()) {
+					searchVariables = new IntVar[v.length - 1];
+					for (int i = 0; i < v.length - 1; i++) {
+						searchVariables[i] = v[i + 1];
+					}
+				} else {
+					searchVariables = v;
+				}
+				return v[0];
+			} else {
+				System.err.println("Zero length list of variables for labeling");
+				return new IntVar(store);
+			}
+		}
 
-        /**
-         * example value selection; indomain_min
-         */
-        int calculateMiddlePoint(IntVar v) {
-            return (v.max() + v.min()) / 2;
-        }
+		/**
+		 * example value selection; indomain_min
+		 */
+		int calculateMiddlePoint(IntVar v) {
+			return (v.max() + v.min()) / 2;
+		}
 
-        /**
-         * example constraint assigning a selected value
-         */
-        public PrimitiveConstraint getConstraint() {
-            return new XlteqC(var, c);
-        }
-    }
+		/**
+		 * example constraint assigning a selected value
+		 */
+		public PrimitiveConstraint getConstraint() {
+			return new XlteqC(var, c);
+		}
+	}
 }
